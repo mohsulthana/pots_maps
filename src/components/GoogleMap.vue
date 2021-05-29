98c0859c5b7afbc7e305ab5fff2ce884d8f47a85
Mohammad Sulthan <muhammadsultana@outlook.com>
2021-05-29 10:45:45 +0700

    completion phase 2
    complete table, dynamic region, styling marker and google map, change font

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -4,7 +4,8 @@
       ref="mapRef"
       :center="center"
       :zoom="12"
-      style="width: 100%; height: 400px"
+      id="mapId"
+      style="width: 100%; height: 600px"
     >
       <gmap-marker
         :key="index"
@@ -13,7 +14,12 @@
         :clickable="true"
         @click="toggleInfoWindow(m, index)"
       ></gmap-marker>
-      <gmap-info-window :options="infoOptions" :position="infoWindowPos" :opened="infoWindowOpen" @closeclick="infoWindowOpen=false" />
+      <gmap-info-window
+        :options="infoOptions"
+        :position="infoWindowPos"
+        :opened="infoWindowOpen"
+        @closeclick="infoWindowOpen = false"
+      />
     </GmapMap>
   </div>
 </template>
@@ -28,31 +34,14 @@ export default {
   },
   data () {
     return {
+      organizer: '',
       infoOptions: {
-        content: '<div id="content">' +
-      '<div id="siteNotice">' +
-      '</div>' +
-      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
-      '<div id="bodyContent">' +
-      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      'sandstone rock formation in the southern part of the ' +
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
-      'south west of the nearest large town, Alice Springs; 450&#160;km ' +
-      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
-      'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
-      'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
-      'Aboriginal people of the area. It has many springs, waterholes, ' +
-      'rock caves and ancient paintings. Uluru is listed as a World ' +
-      'Heritage Site.</p>' +
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-      'https://en.wikipedia.org/w/index.php?title=Uluru</a> ' +
-      '(last visited June 22, 2009).</p>' +
-      '</div>' +
-      '</div>',
+        // content: "<div style='float:left'><img src='http://i.stack.imgur.com/g672i.png'></div><div style='float:right; padding: 10px;'><b>Title</b><br/>123 Address<br/> City,Country</div>",
+        content: '',
         // optional: offset infowindow so it visually sits nicely on top of our marker
         pixelOffset: {
           width: 0,
-          height: -35
+          height: -15
         }
       },
       currentMidx: null,
@@ -72,6 +61,7 @@ export default {
       const geocoder = new google.maps.Geocoder()
 
       this.events.forEach((element) => {
+        console.log(element)
         geocoder.geocode({ address: element.address }, (results, status) => {
           if (status === google.maps.GeocoderStatus.OK) {
             // things should include
@@ -89,7 +79,14 @@ export default {
             this.markers.push({
               lat: latitude,
               lng: longitude,
-              organizer: element.organizer
+              name: element.name,
+              type: element.type,
+              logo: element.logo,
+              organizer: element.organizer,
+              from: element.from,
+              to: element.to,
+              address: element.address,
+              city: element.city
             })
           }
         })
@@ -98,7 +95,23 @@ export default {
   },
   methods: {
     toggleInfoWindow (item, index) {
-      this.infoOptions.content = item.organizer
+      this.infoOptions.content = `
+        <div class="card">
+          <div class="card-body">
+          <div style="float: left;">
+          ${item.logo === undefined ? '<img src="./assets/image.png" alt="Default Logo" height="80" class="mr-3">' : `<img src=${item.logo} height="80" class="rounded-circle">`}
+
+          </div>
+          <div style="float: left;">
+                <h4>${item.name}</h4>
+              <p class="font-weight-bold"> ${item.type}</p>
+              <p class="font-weight-bold">${item.from} - ${item.to}</p>
+              <h6>${item.organizer}</h6>
+              <h6>${item.address} - ${item.city}</h6>
+          </div>
+          </div>
+        </div>
+      `
       this.infoWindowPos = item
       this.infoWindowOpen = true
 
@@ -128,3 +141,12 @@ export default {
   }
 }
 </script>
+
+<style>
+.card,
+.gm-style .gm-style-iw-c,
+.gm-style-iw-t::after {
+  background-color: #660404 !important;
+  color: white;
+}
+</style>
--- src/views/List.vue
+++ src/views/List.vue
@@ -1,24 +1,49 @@
 <template>
-  <b-container>
+  <b-container fluid>
     <b-row>
-      <b-col align="end">
-        <b-icon-funnel font-scale="2.2" class="pt-1" />
-        <b-dropdown text="Select Region">
-          <b-dropdown-item v-for="(value, index) in dropdownItems" :key="index">{{ value.text }}</b-dropdown-item>
-        </b-dropdown>
+      <b-col align-self="end" offset-md="10" cols="2">
+        <b-modal id="modal-1" title="BootstrapVue">
+          <p class="my-4">Hello from modal!</p>
+        </b-modal>
+        <b-input-group>
+          <!-- <b-input-group-prepend>
+            <b-icon-funnel v-b-modal.modal-1 font-scale="2.2" class="pt-1" />
+          </b-input-group-prepend> -->
+
+          <b-form-select v-model="regionSelected">
+            <!-- This slot appears above the options from 'options' prop -->
+            <template #first>
+              <b-form-select-option :value="null" disabled
+                >-- Please select an option --</b-form-select-option
+              >
+            </template>
+            <b-form-select-option
+              :value="value.key"
+              v-for="(value, index) in dropdownItems"
+              :key="index"
+              >{{ value.text }}</b-form-select-option
+            >
+          </b-form-select>
+        </b-input-group>
       </b-col>
     </b-row>
 
     <b-tabs content-class="mt-3">
       <b-tab title="List View" active>
-        <div class="row">
+        <b-row align-h="center" v-if="regionSelected.length < 1">
+          <b-col align-self="center">
+            <h2 style="color: #660404; font-style: italic">Please select your region...</h2>
+          </b-col>
+        </b-row>
+        <b-row v-else>
           <div class="col-lg">
-            <h4>Please select your region...</h4>
             <div>
               <b-table
+              id="my-table"
                 responsive
                 striped
                 hover
+                show-empty
                 :busy="isBusy"
                 :fields="fields"
                 :items="list"
@@ -26,25 +51,29 @@
                 <template #cell(no)="data">
                   {{ data.index + 1 }}
                 </template>
-                <template #cell(name)="data">
-                  <p class="text-left">
+                <template #cell(name)="data" class="text-left text-weight-bold">
+                  <h6 class="text-left">
+                    <img v-if="data.item.logo === undefined" src="@/assets/image.png" alt="Default Logo" height="80" class="mr-3">
                     <img
+                    v-else
                       :src="data.item.logo"
                       height="80"
-                      class="rounded"
-                      alt="logoUrl"
+                      class="rounded-circle mr-3"
+                      alt="Event logo"
                     />
-                    {{ data.item.name }} <br />{{ data.item.type }}
-                  </p>
+                    {{ data.item.name }}
+                  </h6>
                 </template>
                 <template #cell(organizer)="data">
                   <p>{{ data.value }}</p>
                 </template>
                 <template #cell(date)="data">
                   {{ data.item.date }} <br />
-                  {{ data.item.address }}, {{data.item.city}}
+                  {{ data.item.address }}, {{ data.item.city }}
                 </template>
-                <template #cell()="" style="display: none"> </template>
+                <template #cell(type)="data">
+                  {{ data.item.type }} <br />
+                </template>
                 <template #table-busy>
                   <div class="text-center text-danger my-2">
                     <b-spinner class="align-middle"></b-spinner>
@@ -54,7 +83,7 @@
               </b-table>
             </div>
           </div>
-        </div>
+        </b-row>
       </b-tab>
       <b-tab title="Map View" lazy>
         <google-map-view :events="list" />
@@ -71,49 +100,96 @@ export default {
   data () {
     return {
       list: [],
-      fields: ['no', 'name', 'organizer', 'date'],
+      fields: ['no', 'name', 'organizer', 'date', 'type'],
       dropdownItems: [],
-      isBusy: false
+      isBusy: false,
+      regionSelected: '',
+      isRegionSelected: false
     }
   },
   components: {
     GoogleMapView
   },
+  computed: {},
+  watch: {
+    regionSelected: function (newValue, oldValue) {
+      this.regionSelected = newValue
+      this.getEvents()
+    }
+  },
   methods: {
+    capitalizeWord (string) {
+      return string.charAt(0).toUpperCase() + string.slice(1)
+    },
+    isOneWord (string) {
+      return string.length > 0 && string.split('\\s+').length === 1
+    },
     getDropdownItems () {
-      axios.get('events/regions/GBR')
-        .then((response) => {
-          for (const key in response.data.results) {
-            if (Object.hasOwnProperty.call(response.data.results, key)) {
-              const element = response.data.results[key]
-              this.dropdownItems.push({
-                key: element,
-                text: element
-              })
-            }
+      axios.get('events/regions/GBR').then((response) => {
+        for (const key in response.data.results) {
+          if (Object.hasOwnProperty.call(response.data.results, key)) {
+            const element = response.data.results[key]
+            this.dropdownItems.push({
+              key: key,
+              text: element
+            })
           }
-        })
+        }
+      })
     },
     getEvents () {
       this.isBusy = true
       axios
-        .get('events/local/GBR/ENG_GLN/1')
+        .get(`events/ukata/GBR/${this.regionSelected}/1`)
         .then((response) => {
+          const dateFormatOptions = {
+            month: '2-digit',
+            day: '2-digit'
+          }
           response.data.results.forEach((element) => {
+            const timestampStarted =
+              (element.from.seconds + element.from.nanos) * 1000
+            const timestampEnded =
+              (element.to.seconds + element.to.nanos) * 1000
+
+            if (element.type === 'MILONGA_CLASS') {
+              element.type = 'Milonga & Class/Workshop'
+            }
+            if (element.type === 'PRACTICA_CLASS') {
+              element.type = 'Practica & Class/Workshop'
+            }
+            if (element.type === 'FESTIVAL_MARATHON') {
+              element.type = 'Festival / Marathon'
+            }
+            if (element.type === 'FESTIVAL_CHAMPIONSHIP') {
+              element.type = 'Festival / Championship'
+            }
+
+            if (this.isOneWord(element.type)) {
+              element.type =
+                element.type.charAt(0).toUpperCase() +
+                element.type.slice(1).toLowerCase()
+            }
+
             // logoUrl, name, from - to, city, address & postCode, type, associationName
             this.list.push({
               logo: element.logoUrl,
               name: element.name,
               organizer: element.associationName,
-              date: `From: ${new Date(element.from.seconds).getDate()}/${
-                new Date(element.from.seconds).getMonth() + 1
-              } at ${new Date(element.from.seconds).toLocaleString('en-US', {
+              from: `${new Date(timestampStarted).toLocaleString('en-US', dateFormatOptions)} at ${new Date(timestampStarted).toLocaleString('en-US', {
                 hour: 'numeric',
                 minute: 'numeric',
                 hour12: true
-              })} - To: ${new Date(element.to.seconds).getDate()}/${
-                new Date(element.to.seconds).getMonth() + 1
-              } at ${new Date(element.to.seconds).toLocaleString('en-US', {
+              })}`,
+              to: `${new Date(timestampEnded).toLocaleString('en-US', dateFormatOptions)} at ${new Date(timestampEnded).toLocaleString('en-US', {
+                hour: 'numeric',
+                minute: 'numeric',
+                hour12: true
+              })}`,
+              date: `From: ${new Date(timestampStarted).toLocaleString('en-US', dateFormatOptions)} at ${new Date(timestampStarted).toLocaleString('en-US', {
+                hour: 'numeric',
+                minute: 'numeric'
+              })} - To: ${new Date(timestampEnded).toLocaleString('en-US', dateFormatOptions)} at ${new Date(timestampEnded).toLocaleString('en-US', {
                 hour: 'numeric',
                 minute: 'numeric',
                 hour12: true
@@ -124,14 +200,30 @@ export default {
             })
           })
         })
+        .catch((error) => {
+          this.list = []
+          console.log(error)
+        })
         .finally(() => {
+          this.$root.$emit('bv::refresh::table', 'my-table')
           this.isBusy = false
         })
     }
   },
   created () {
-    this.getEvents()
     this.getDropdownItems()
+  },
+  mounted () {
+    this.$root.$on('bv::dropdown::show', (bvEvent) => {
+      console.log('Dropdown is about to be shown', bvEvent)
+    })
   }
 }
 </script>
+
+<style scoped>
+td > h6 {
+  font-weight: 700;
+  font-size: 1.3em;
+}
+</style>
