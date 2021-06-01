c55aa1ce2119e66b8ce4fe3bb1f6d344b7cd90c9
Mohammad Sulthan <muhammadsultana@outlook.com>
2021-06-01 12:00:10 +0700

    bring current page to root component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/list">List</router-link> |
+      <list />
     </div>
-    <router-view/>
   </div>
 </template>
+
+<script>
+import List from '@/views/List.vue'
+
+export default {
+  components: {
+    List
+  }
+}
+</script>
 
 <style>
 #app {
@@ -22,10 +29,10 @@
   padding: 30px;
 }
 
-#nav a {
+/* #nav a {
   font-weight: bold;
   color: #2c3e50;
-}
+} */
 
 #nav a.router-link-exact-active {
   color: #42b983;
